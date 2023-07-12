6e4e6f584404f7786a17a8392675ca208b170e22
WyattBensman <wyattbensman5@gmail.com>
2023-07-12 18:54:23 -0400

    Added initial Files + Directions as Comments

---
+++ script.js
@@ -0,0 +1,24 @@
+// Three Global Variables
+// 1 for the API Key [Paste Key into this]
+// 1 for the Base URL which is the Https:.com
+// Search History Array
+
+// Function: Display Search History
+// Loop over the History Array & count down so most recent serach is at the top
+
+// Function: Update the search history in local storage
+
+// Function: Get the search history in local storage
+
+// Function: Get the current weather data from fetch request
+
+// Function: Display 5 day forecast data from fetch request
+// need icon, temp, humidity, date, wind speed
+
+// Create elements for each of the above
+
+// Function: Display 5 day forecast data frmoo fetch request for each card
+
+// Function: Specifically for geolocation with latitude & longitude and use function above to make calls here - callback function in here
+
+// Function: Event listener for search butttoine
